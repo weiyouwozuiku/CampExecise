67b9ebd6d020b3cb4de2556caf5206e9b6fb6330
robertwang <19681022+weiyouwozuiku@users.noreply.github.com>
2022-09-10 09:14:53 +0800

    save

--- Chap1/proxy_sock5.go
+++ Chap1/proxy_sock5.go
@@ -2,7 +2,18 @@ package chap1
 
 import (
 	"bufio"
+	"fmt"
+	"io"
+	"log"
 	"net"
+)
+
+const (
+	socks5Ver = 0x05
+	cmdBind   = 0x01
+	atypIPV4  = 0x01
+	atypeHOST = 0x03
+	atypeIPV6 = 0x04
 )
 
 func Server() {
@@ -21,6 +32,11 @@ func Server() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	reader := bufio.NewReader(conn)
+	if err := auth(reader, conn); err != nil {
+		log.Printf("client %v auth failed:%v", conn.RemoteAddr(), err)
+		return
+	}
+	log.Println("auth success")
 	for {
 		b, err := reader.ReadByte()
 		if err != nil {
@@ -31,4 +47,33 @@ func handleConnection(conn net.Conn) {
 			break
 		}
 	}
+}
+
+func auth(reader *bufio.Reader, conn net.Conn) error {
+	ver, err := reader.ReadByte()
+	if err != nil {
+		return fmt.Errorf("read ver failed:%w", err)
+	}
+	if ver != socks5Ver {
+		return fmt.Errorf("not supported ver:%v", ver)
+	}
+	methodSize, err := reader.ReadByte()
+	if err != nil {
+		return fmt.Errorf("read methodSize failed:%w", err)
+	}
+	method := make([]byte, methodSize)
+	_, err = io.ReadFull(reader, method)
+	if err != nil {
+		return fmt.Errorf("read method failed:%w", err)
+	}
+	log.Println("ver", ver, "method", method)
+	_, err = conn.Write([]byte{socks5Ver, 0x00})
+	if err != nil {
+		return fmt.Errorf("write failed:%w", err)
+	}
+	return nil
+}
+
+func connect(conn net.Conn) error {
+	return nil
 }
--- Chap1/test/Chap1_test.go
+++ Chap1/test/Chap1_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-// 可以使用nc 127.0.0.1 1088实验
+// 可以使用nc 127.0.0.1 1080实验
 func Test_Server(t *testing.T) {
 	t.Log("demo")
 	chap1.Server()
