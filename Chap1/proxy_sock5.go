ccd1dfa93c375b899cf17a8408ccd1a24e7bbdfb
robertwang <19681022+weiyouwozuiku@users.noreply.github.com>
2022-09-11 17:30:55 +0800

    save

--- Chap1/proxy_sock5.go
+++ Chap1/proxy_sock5.go
@@ -2,6 +2,9 @@ package chap1
 
 import (
 	"bufio"
+	"context"
+	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -24,12 +27,13 @@ func Server() {
 	for {
 		conn, err := server.Accept()
 		if err != nil {
-			panic(err)
+			log.Printf("Accept failed %v", err)
+			continue
 		}
-		go handleConnection(conn)
+		go process(conn)
 	}
 }
-func handleConnection(conn net.Conn) {
+func process(conn net.Conn) {
 	defer conn.Close()
 	reader := bufio.NewReader(conn)
 	if err := auth(reader, conn); err != nil {
@@ -46,10 +50,23 @@ func handleConnection(conn net.Conn) {
 		if err != nil {
 			break
 		}
+		if err = connect(reader, conn); err != nil {
+			log.Printf("client %v connect failed:%v", conn.RemoteAddr(), err)
+		}
 	}
 }
 
 func auth(reader *bufio.Reader, conn net.Conn) error {
+	// +----+----------+----------+
+	// |VER | NMETHODS | METHODS  |
+	// +----+----------+----------+
+	// | 1  |    1     | 1 to 255 |
+	// +----+----------+----------+
+	// VER: 协议版本，socks5为0x05
+	// NMETHODS: 支持认证的方法数量
+	// METHODS: 对应NMETHODS，NMETHODS的值为多少，METHODS就有多少个字节。RFC预定义了一些值的含义，内容如下:
+	// X’00’ NO AUTHENTICATION REQUIRED
+	// X’02’ USERNAME/PASSWORD
 	ver, err := reader.ReadByte()
 	if err != nil {
 		return fmt.Errorf("read ver failed:%w", err)
@@ -74,6 +91,91 @@ func auth(reader *bufio.Reader, conn net.Conn) error {
 	return nil
 }
 
-func connect(conn net.Conn) error {
+func connect(reader *bufio.Reader, conn net.Conn) error {
+	// +----+-----+-------+------+----------+----------+
+	// |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
+	// +----+-----+-------+------+----------+----------+
+	// | 1  |  1  | X'00' |  1   | Variable |    2     |
+	// +----+-----+-------+------+----------+----------+
+	// VER 版本号，socks5的值为0x05
+	// CMD 0x01表示CONNECT请求
+	// RSV 保留字段，值为0x00
+	// ATYP 目标地址类型，DST.ADDR的数据对应这个字段的类型。
+	//   0x01表示IPv4地址，DST.ADDR为4个字节
+	//   0x03表示域名，DST.ADDR是一个可变长度的域名
+	// DST.ADDR 一个可变长度的值
+	// DST.PORT 目标端口，固定2个字节
+	buf := make([]byte, 4)
+	_, err := io.ReadFull(reader, buf)
+	if err != nil {
+		return fmt.Errorf("reader header failed:%w", err)
+	}
+	ver, cmd, _, atvp := buf[0], buf[1], buf[2], buf[3]
+	if ver != socks5Ver {
+		return fmt.Errorf("not support ver:%v", ver)
+	}
+	if cmd != cmdBind {
+		return fmt.Errorf("not support cmd:%v", err)
+	}
+	addr := ""
+	switch atvp {
+	case atypIPV4:
+		_, err := io.ReadFull(reader, buf)
+		if err != nil {
+			return fmt.Errorf("read atyp failed:%w", err)
+		}
+		addr = fmt.Sprintf("%d.%d.%d.%d", buf[0], buf[1], buf[2], buf[3])
+	case atypeIPV6:
+		return errors.New("IPV6 : not supported yet")
+	case atypeHOST:
+		hostSize, err := reader.ReadByte()
+		if err != nil {
+			return fmt.Errorf("read hostSzie failed:%w", err)
+		}
+		host := make([]byte, hostSize)
+		_, err = io.ReadFull(reader, host)
+		if err != nil {
+			return fmt.Errorf("read host failed:%w", err)
+		}
+		addr = string(host)
+	default:
+		return errors.New("invalid atyp")
+	}
+	if _, err = io.ReadFull(reader, buf[:2]); err != nil {
+		return fmt.Errorf("read port failed:%w", err)
+	}
+	port := binary.BigEndian.Uint16(buf[:2])
+	dest, err := net.Dial("tcp", fmt.Sprintf("%v:%v", addr, port))
+	if err != nil {
+		return fmt.Errorf("dial dst failed:%w", err)
+	}
+	defer dest.Close()
+	log.Println("dial", addr, port)
+	// +----+-----+-------+------+----------+----------+
+	// |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
+	// +----+-----+-------+------+----------+----------+
+	// | 1  |  1  | X'00' |  1   | Variable |    2     |
+	// +----+-----+-------+------+----------+----------+
+	// VER socks版本，这里为0x05
+	// REP Relay field,内容取值如下 X’00’ succeeded
+	// RSV 保留字段
+	// ATYPE 地址类型
+	// BND.ADDR 服务绑定的地址
+	// BND.PORT 服务绑定的端口DST.PORT
+	_, err = conn.Write([]byte{0x05, 0x00, 0x00, 0x01, 0, 0, 0, 0, 0, 0})
+	if err != nil {
+		return fmt.Errorf("write failed: %w", err)
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go func() {
+		io.Copy(dest, reader)
+		cancel()
+	}()
+	go func() {
+		io.Copy(conn, dest)
+		cancel()
+	}()
+	<-ctx.Done()
 	return nil
 }
